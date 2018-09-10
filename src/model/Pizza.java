40704d8cd099497f4d2dadb39cc7a29a7c639b6a
Ainsleigh Hill <Ainsleigh@dhcp-128-189-244-109.ubcsecure.wireless.ubc.ca>
2018-09-10 12:35:09 -0700

    First commit

---
+++ src/model/Pizza.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Pizza {
+    private int numToppings;
+    private String name;
+
+    public Pizza (String name){
+        numToppings = 0;
+    }
+
+    public void addATopping(){
+        numToppings--;
+    }
+
+    public void removeATopping(){
+        numToppings++;
+    }
+
+    public void print(){
+        System.out.println(name + " has "+numToppings+" toppings");
+    }
+}
---
+++ src/ui/Main.java
@@ -0,0 +1,17 @@
+package ui;
+
+import model.Pizza;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+        Pizza pizza1 = new Pizza("pizza1");
+        Pizza pizza2 = new Pizza("pizza2");
+        pizzas.add(pizza1);
+        pizzas.add(pizza2);
+        pizzas.get(0).print();
+        pizzas.get(1).print();
+    }
+}
